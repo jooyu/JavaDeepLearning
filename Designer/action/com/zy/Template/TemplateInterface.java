dca28cf2d3c207dd77574604e522f7375907cdc4
zhuyu <1359231385@qq.com>
2016-10-17 22:43:54 +0800

    模板模式

---
+++ Designer/action/com/zy/Template/TemplateInterface.java
@@ -0,0 +1,6 @@
+package com.zy.Template;
+
+//模板方法
+public interface TemplateInterface { 
+	public void execute(); 
+	}
---
+++ Designer/jvm/com/zy/jvm/Widget.java
@@ -0,0 +1,8 @@
+package com.zy.jvm;
+
+public class Widget {
+public synchronized void dosomething()
+{
+	System.out.println("hello");
+}
+}
